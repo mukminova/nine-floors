d332c62098ef645749272fa1796c4ae4dc6fabe5
Maks Gryaznov <lkdnvc@gmail.com>
2013-07-31 10:52:49 +0400

    registration

--- protected/views/site/registration.php
+++ protected/views/site/registration.php
@@ -1,22 +1,23 @@
 <h1>Регистрация</h1>
 <form name="registration" method="POST">
-  <p>
-    <label>Ваш логин:<br></label>
-    <input name="login" type="text" >
-  </p>
-  <p>
-    <label>Ваш пароль:<br></label>
-    <input name="password" type="password" >
-  </p>
-  <p>
-    <label>Повторите пароль:<br></label>
-    <input name="repeat_password" type="password" >
-  </p>
-  <?php if (!empty($errors)): ?>
-    <div class = "errors">
-      <?php echo $errors;
-      ?>
-    </div>
-  <?php endif; ?>
-  <input type="submit" name="submit" value="Зарегистрироваться"/>
+    <p>
+        <label>Ваш логин:<br></label>
+        <input name="login" type="text" >
+    </p>
+    <p>
+        <label>Ваш пароль:<br></label>
+        <input name="password" type="password" >
+    </p>
+    <p>
+        <label>Повторите пароль:<br></label>
+        <input name="repeat_password" type="password" >
+    </p>
+    <?php if (!empty($errors)): ?>
+        <?php foreach ($errors as $error): ?>
+            <div class = "errors">
+                <?php  echo $error['0'];?>
+            </div>
+        <?php endforeach; ?>
+    <?php endif; ?>
+    <input type="submit" name="submit" value="Зарегистрироваться"/>
 </form>
